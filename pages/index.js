6e0c936682a15c6cc9c16be324312f92ae0524e8
Ross Basri <rossbasri@Rosss-MBP.fios-router.home>
2022-03-22 16:51:46 -0400

    added jobs board

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,9 @@ export default function Home() {
         <h1 className={styles.title}>
           WAGMI.
         </h1>
+        <h2>
+          <a href="https://jobs.gusto.com/boards/airdrop-labs-de232069-6209-49e9-8597-63ac9d858075" target="_blank" className={styles.link}>[We're Hiring]</a>
+        </h2>
       </main>
     </div>
   )
